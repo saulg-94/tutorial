025138ab906bc1edde3f7ce215d8ffb756ae6188
Saul Gonzalez <94.gonzlezsaul@gmail.com>
2022-06-24 10:14:36 -0400

    finished app.js for questions project

--- smallProjects/questions-setup/app.js
+++ smallProjects/questions-setup/app.js
@@ -1,2 +1,29 @@
-//using selectors inside the element
-// traversing the dom
+//using selectors inside the element - option 2 the better option
+
+const questions = document.querySelectorAll('.question');
+
+questions.forEach(function (question) {
+  //   console.log(question);
+  const btn = question.querySelector('.question-btn');
+  //   console.log(btn);
+  btn.addEventListener('click', function () {
+    questions.forEach(function (item) {
+      if (item !== question) {
+        item.classList.remove('show-text');
+      }
+    });
+
+    question.classList.toggle('show-text');
+  });
+});
+
+// traversing the dom - option 1 for adding functionality
+
+// const btns = document.querySelectorAll('.question-btn');
+
+// btns.forEach(function (btn) {
+//   btn.addEventListener('click', function (e) {
+//     const question = e.currentTarget.parentElement.parentElement;
+//     question.classList.toggle('show-text');
+//   });
+// });
