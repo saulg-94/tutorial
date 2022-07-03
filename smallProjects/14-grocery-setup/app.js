2583aab6977dd8b5c6479f86773a4d3330bb3492
saulg-94 <94.gonzlezsaul@gmail.com>
2022-07-03 13:08:57 -0400

    finished removeFromLocalStorage functionality

--- smallProjects/14-grocery-setup/app.js
+++ smallProjects/14-grocery-setup/app.js
@@ -117,7 +117,7 @@ function deleteItem(e) {
   displayAlert("item removed", "danger");
   setBackToDefault();
   // remove from local storage
-  //   removeFromLocalStorage(id);
+  removeFromLocalStorage(id);
 }
 
 // set back to default
@@ -131,16 +131,29 @@ function setBackToDefault() {
 
 function addToLocalStorage(id, value) {
   const grocery = { id, value };
-  let items = localStorage.getItem("list")
-    ? JSON.parse(localStorage.getItem("list"))
-    : [];
-  console.log(items);
+  let items = getLocalStorage();
 
   items.push(grocery);
   localStorage.setItem("list", JSON.stringify(items));
 }
-function removeFromLocalStorage(id) {}
+
+function removeFromLocalStorage(id) {
+  let items = getLocalStorage();
+
+  items = items.filter(function (item) {
+    if (item.id !== id) {
+      return item;
+    }
+  });
+  localStorage.setItem("list", JSON.stringify(items));
+}
+
 function editLocalStorage(id, value) {}
+function getLocalStorage() {
+  return localStorage.getItem("list")
+    ? JSON.parse(localStorage.getItem("list"))
+    : [];
+}
 // localStorage API
 // setItem
 // getItem
