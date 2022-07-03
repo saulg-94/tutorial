cf114c312e34c57d754f3bf1177fd73f9ccd4fc6
saulg-94 <94.gonzlezsaul@gmail.com>
2022-07-03 12:32:23 -0400

    finished addToLocalStorage functionality

--- smallProjects/14-grocery-setup/app.js
+++ smallProjects/14-grocery-setup/app.js
@@ -130,8 +130,25 @@ function setBackToDefault() {
 // ****** LOCAL STORAGE **********
 
 function addToLocalStorage(id, value) {
-  //   console.log("added to local storage");
+  const grocery = { id, value };
+  let items = localStorage.getItem("list")
+    ? JSON.parse(localStorage.getItem("list"))
+    : [];
+  console.log(items);
+
+  items.push(grocery);
+  localStorage.setItem("list", JSON.stringify(items));
 }
 function removeFromLocalStorage(id) {}
 function editLocalStorage(id, value) {}
+// localStorage API
+// setItem
+// getItem
+// removeItem
+// save as strings
+
+// localStorage.setItem("orange", JSON.stringify(["item", "item2"]));
+// const oranges = JSON.parse(localStorage.getItem("orange"));
+// console.log(oranges);
+// localStorage.removeItem("orange");
 // ****** SETUP ITEMS **********
