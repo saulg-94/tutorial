f4a92a795b5848defdd1a4e2269b6089a8f12154
saulg-94 <94.gonzlezsaul@gmail.com>
2022-07-13 21:33:31 -0400

    initial commit for oop project setup

--- lectureVideos/app.js
+++ lectureVideos/app.js
@@ -1,1 +1,29 @@
+// nested objects
+// set variable as property value
+// dot notation vs bracket notation
 
+const age = 60;
+
+const person = {
+  name: "john",
+  age: age,
+  married: true,
+  siblings: ["anna", "peter"],
+  greet: function (name) {
+    console.log(`Hello, my name is ${name}`);
+  },
+  sayHello(name) {
+    console.log(`Hello, my name is ${name}`);
+  },
+  job: {
+    title: "developer",
+    company: {
+      name: "coding addict",
+      address: "123 main street",
+    },
+  },
+};
+
+console.log(person.job.title);
+console.log(person.job.company.address);
+console.log(person);
